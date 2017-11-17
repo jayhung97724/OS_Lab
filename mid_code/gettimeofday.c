293af788ac0f7065a24c35d5599ded230c271bbe
YoYo <jayhung97724@gmail.com>
2017-11-17 11:59:59 +0800

    mid done happyls!

---
+++ mid_code/gettimeofday.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <sys/time.h>
+#include <unistd.h>
+ 
+ 
+int delay(int time)
+{
+    int i,j;
+   
+    for(i =0;i<time;i++)
+        for(j=0;j<5000;j++)
+            ;
+}
+ 
+int main()
+{
+        struct  timeval start;
+        struct  timeval end;
+       
+        unsigned  long diff;
+        gettimeofday(&start,NULL);
+        delay(10);
+        gettimeofday(&end,NULL);
+        diff = end.tv_usec-start.tv_usec;
+        printf("the difference is %ld\n",diff);
+        return 0;
+       
+}
