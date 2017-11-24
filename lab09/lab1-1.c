163c2739643a17b14c3d24030254a76519dc000e
yoyo <jayhung97724@gmail.com>
2017-11-24 11:38:16 +0800

    lab9 2017/11/24

---
+++ lab09/lab1-1.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
+
+int main() {
+	
+	pid_t new_pid;
+	new_pid = fork();
+
+	switch(new_pid) {
+	    case -1:
+		printf("fork error!\n");
+		break;
+	    case 0: // create a child process
+		printf("----------This is child process----------\n");
+		printf("Pid:        %d  ", getpid());
+		printf("Parent Pid: %d\n", getppid());
+		printf("----------I'm killing my parent----------\n");
+		int i;
+		for(i=0; i<5 ; i++){
+			printf(". ");
+			sleep(1);
+		}
+		kill(getppid(), SIGTERM);
+		break;
+	    default:
+		printf("----------This is parent process----------\n");
+		printf("Pid:        %d\n", getpid());
+		while(1){;}
+		break;
+	}
+	return 0;
+}
